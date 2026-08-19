e2705a1a33d4f12d47676bea8fbe6cf9e4b2a9da
Lucía Ruiz Castellani <luciaruizcastellani@gmail.com>
2026-08-19 11:16:50 -0300

    ejercicio 5

--- codigo/principal.py
+++ codigo/principal.py
@@ -1,2 +1,19 @@
 # codigo/principal.py
 print("Primer programa del Proyecto Integrador")
+texto = "Programacion en Python"
+
+#a
+print(texto[0])
+print(texto[-1])
+
+#b
+print(texto[0:12])
+
+#c
+print(texto[::-1])
+
+#d
+if "Python" in texto:
+    print("La palabra 'Python' se encuentra en el texto.")
+else:
+    print("La palabra 'Python' no se encuentra en el texto.")
